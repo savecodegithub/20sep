dda62e11a0bdd03972281dc5024b03443d6bdb0b
savecodegithub <113486296+savecodegithub@users.noreply.github.com>
2022-09-20 07:57:05 +0530

    Add files via upload

---
+++ hellow.sh
@@ -0,0 +1,13 @@
+echo "Hello world,This is my first shell script"
+echo "Enter your number of choice"
+read a
+echo "Enteryour age"
+read b
+
+var=$((a*b))
+
+echo $var
+
+echo "the numbers you gave are $a and $b "
+
+echo "the code is perfct"
